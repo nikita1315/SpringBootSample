96b50750a76dc9c8e634274af879a79eba5b748e
nikita1315 <you@example.com>
2019-12-14 13:57:33 +0300

    ///

--- src/main/resources/static/js/allFilms.js
+++ src/main/resources/static/js/allFilms.js
@@ -5,10 +5,10 @@ function test () {
         type: 'get',
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
-        success: function (film) {
-            console.log(film);
-            console.log(film.title);
-            console.log(film.rating);
+        success: function (films) {
+            console.log(films);
+            console.log(films.title);
+            console.log(films.rating);
             alert('success');
         },
         error: function (response) {
@@ -21,5 +21,39 @@ function test () {
 
 
 $(document).ready(function () {
-    alert("loaded")
+    alert("loaded");
+
+    $.ajax({
+        url: '/api/films/all',
+        type: 'get',
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: function (films) {
+
+            console.log(films);
+
+            for (let i = 0; i < films.length; i++) {
+                $("#forAdd").before(
+                    "<tr>"+
+                    "<td>"+films[i].id+"</td>"+
+                    "<td>"+films[i].title+"</td>"+
+                    "<td>"+films[i].rating+"</td>"+
+                    "<td>"+films[i].age+"</td>"+
+                    "<td>"+
+                    "<a type=\"button\" class=\"btn btn-primary\" href=\"/films/edit?id= " + films[i].id+" \">Edit</a>"+
+                    "</td>"+
+                    "<td>"+
+                    "<a type=\"button\" class=\"btn btn-danger\" href=\"/films/delete?id=" + films[i].id +" \">Delete</a>"+
+                    "</td>"+
+                    "</tr>")
+
+            }
+
+
+        },
+        error: function (response) {
+            alert('error');
+        }
+    });
+
 })
